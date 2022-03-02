a810a4bd2e69472092eecad3cabfb0f4c4ffb959
pocketberserker <pocketberserker@gmail.com>
2022-03-02 16:04:15 +0900

    Wrap Serializer

---
+++ standalone-project/Benchmarks/Serializers/Serializer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+namespace Benchmarks.Serializers
+{
+    public abstract class Serializer
+    {
+        public abstract object Serialize<T>(T input);
+        public abstract T Deserialize<T>(object input);
+    }
+}
