e056e8d7a09d0004d3a64f7fceb7eaed648d0af7
pocketberserker <pocketberserker@gmail.com>
2022-03-02 16:50:50 +0900

    Show memory allocation diagnoser

---
+++ standalone-project/Benchmarks/BenchmarkConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+
+namespace Benchmarks
+{
+    public class BenchmarkConfig : ManualConfig
+    {
+        public BenchmarkConfig()
+        {
+            this.AddDiagnoser(MemoryDiagnoser.Default);
+        }
+    }
+}
